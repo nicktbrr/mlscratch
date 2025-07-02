510187784ecd22538058aed2dcb72f5e9641e3b8
Nicholas Barsi-Rhyne <nicholasbarsi-rhyne@Nicholass-MacBook-Air.local>
2025-07-02 17:50:23 -0400

    starting LR

---
+++ classic-regression/linear_regression.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+class LinearRegression:
+    def __init__(self, learning_rate=0.001, n_iters=1000):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
