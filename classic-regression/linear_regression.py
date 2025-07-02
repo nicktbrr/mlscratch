54c78e0436f29d64cd0665df2b818eb84acdda63
Nicholas Barsi-Rhyne <nicholasbarsi-rhyne@Nicholass-MacBook-Air.local>
2025-07-02 18:18:31 -0400

    almost done

--- classic-regression/linear_regression.py
+++ classic-regression/linear_regression.py
@@ -2,11 +2,41 @@ import numpy as np
 import pandas as pd
 
 class LinearRegression:
-    def __init__(self, learning_rate=0.001, n_iters=1000):
+    def __init__(self, learning_rate=0.001, n_iters=1000, bias=True):
+        self.learning_rate = learning_rate
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = bias
+        self.N = None
+
+    def fit(self, X: np.array, y: np.array) -> bool:
+        if self.bias:
+            X = np.hstack([np.ones((X.shape[0], 1)), X])
+        if not self.N:
+            self.N = X.shape[0]
+        self.weights = np.zeros(X.shape[1])
+        for _ in range(self.n_iters):
+            y_hat = np.dot(X, self.weights)
+            break
+
+
+    def predict(self, X: np.array):
         pass
 
-    def fit(self, X, y):
+    def _MSE(self, y: np.array, y_hat: np.array) -> np.float64:
+        return np.sum(np.pow(y - y_hat, 2))
+
+    def _MSE_derivative(self, y, y_hat):
         pass
 
-    def predict(self, X):
-        pass
+
+lin = LinearRegression()
+X = np.random.rand(3,2)
+y = np.random.rand(3, 1)
+
+print(X.shape, X)
+print(y.shape, y)
+
+lin.fit(X, y)
+
+print(type(lin._MSE(y,y)))
