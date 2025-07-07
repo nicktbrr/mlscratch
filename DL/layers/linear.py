c7d284545b93690ebb92f6b3fdf7682e66a52129
Nicholas Barsi-Rhyne <nicholasbarsi-rhyne@Nicholass-MacBook-Air.local>
2025-07-07 16:43:15 -0400

    linear done

--- DL/layers/linear.py
+++ DL/layers/linear.py
@@ -3,10 +3,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class linear(nn.Module):
-    def __init__(self,input, output):
-        super(linear).__init__()
-        self.weights = nn.Parameter(torch.randn(input, output))
-        self.bias = nn.Parameter(torch.randn(input))
+    def __init__(self, input: int, output: int):
+        super(linear, self).__init__()
+        self.weights = nn.Parameter(torch.randn(output, input))
+        self.bias = nn.Parameter(torch.randn(output))
     
     def forward(self, input):
         return F.linear(input, self.weights, self.bias)
+    
+
+
+if __name__ == "__main__":
+    input = torch.randn(10)
+    print(input.shape)
+    f = linear(input.shape[0],20)
+    print(f.weights.shape)
+    print(f(input).shape)
