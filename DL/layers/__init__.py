857559e8323a47973fc368e2e7652668a9ddfa5c
Nicholas Barsi-Rhyne <nicholasbarsi-rhyne@Nicholass-MacBook-Air.local>
2025-07-10 16:14:18 -0400

    add inits

---
+++ DL/layers/__init__.py
@@ -0,0 +1,4 @@
+from .linear import Linear
+from .conv import Conv2D
+
+__all__ = ['Linear', 'Conv2D']
---
+++ weight_inits/__init__.py
@@ -0,0 +1,5 @@
+# weight_inits/__init__.py
+from .he import he
+from .xavier import xavier
+
+__all__ = ['he', 'xavier']
